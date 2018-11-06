4d8483db47a26946baea4e22045582338eed0043
Your Name <you@example.com>
2018-11-06 11:41:45 +0100

    addScore, instans af konto

--- src/spil/Account.java
+++ src/spil/Account.java
@@ -1,4 +1,21 @@
 package spil;
-//Søren og Joe arbejder her 10.20-
+
 public class Account {
+    public int score = 1000;
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int addScore(int point){
+        this.score =+ point;
+        if (this.score < 0)
+            this.score = 0;
+        return this.score;
+
+    }
 }
--- src/spil/Player.java
+++ src/spil/Player.java
@@ -1,8 +1,16 @@
 package spil;
+//Søren og Joe arbejder her 10.20-11.40
 
 public class Player {
-    private int score = 1000;
+
+    private Account konto;
     private String name;
+
+    public Player(String name){
+        Account konto = new Account();
+        this.name = name;
+        this.konto = konto;
+    }
 
     public String getName(){
         return this.name;
@@ -12,4 +20,9 @@ public class Player {
         this.name = name;
     }
 
+    //lægger nye points til score
+    public int addScore(int point){
+        int nyScore = konto.addScore(point);
+        return nyScore;
+    }
 }
