ca23274d444dc9ce8625c9a57ce64b1575658f9c
Your Name <you@example.com>
2018-11-06 10:42:50 +0100

    Spiller navn get og set

--- src/spil/Player.java
+++ src/spil/Player.java
@@ -1,4 +1,15 @@
 package spil;
 
 public class Player {
+    private int score = 1000;
+    private String name;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
 }
