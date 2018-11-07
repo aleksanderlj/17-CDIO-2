a33f944410b50418acaa1450069cf921182f5d36
aleksanderlj <aleksanderlj@hotmail.com>
2018-11-07 17:27:11 +0100

    Tilføjet kode til intro af spillet (VIRKER IKKE ENDNU!) //Aleksander

--- src/spil/Goldmine.java
+++ src/spil/Goldmine.java
@@ -10,17 +10,31 @@ public class Goldmine {
         TUI.printLanguage();
 
         // Choose dice
+        boolean correctDieValue;
         do {
             TUI.printChooseDice();
+
             int dieFace1 = TUI.getDieFace(0);
             int dieFace2 = TUI.getDieFace(1);
+            int dieFaceSum = dieFace1 + dieFace2;
+            correctDieValue = (((dieFaceSum <= 12) && (dieFaceSum >= 2)) &&
+                              ((dieFace1 < 12) && (dieFace1 > 0)) &&
+                              ((dieFace2 < 12) && (dieFace2 > 0)));
+
             Game.createDiePair(dieFace1, dieFace2);
-        } while ();
+        } while (!correctDieValue);
 
         // Choose name
         TUI.chooseNames();
 
+        Game.createPlayer(TUI.getName(0));
+        Game.createPlayer(TUI.getName(1));
+
+
         // Play game
+        do {
+
+        } while (!Game.isWinner());
 
         // Win + end game
     }
