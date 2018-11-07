cdd8d22390b2c52a8ea63a3e9200b8cf782b50df
aleksanderlj <aleksanderlj@hotmail.com>
2018-11-07 12:48:54 +0100

    Skelet af controller lavet

--- src/spil/Controller.java
+++ src/spil/Controller.java
@@ -1,4 +1,41 @@
 package spil;
 
+import gui.TUI;
+
+import java.util.Scanner;
+
 public class Controller {
+    public static void main(String[] args){
+        /*
+        Die[] die = new Die[2];
+        Player[] player = new Player[2];
+
+        // Choose Language
+        //...
+
+
+        // Choose dice
+
+        die[0] = new Die(TUI.getFace(1));
+        die[1] = new Die(TUI.getFace(2));
+
+
+        // Choose names
+
+        player[0] = new Player(TUI.getName(1));
+        player[1] = new Player(TUI.getName(2));
+
+
+        //TUI.slag(score(rollSum));
+
+        // Start spil
+
+        do{
+            Game.round(player[0], die[0]);
+            game
+
+
+        }while(!Game.isWinner(player1, player2));
+        */
+    }
 }
