83f6ee650f50852aa6d8e72413ffe300273037bd
Your Name <you@example.com>
2018-11-07 12:44:48 +0100

    Integrationstest af regler. Under 0 points?

---
+++ src/test/GameStub.java
@@ -0,0 +1,8 @@
+package test;
+
+public class GameStub {
+    private int scoreStub(){
+        int score = -100;
+        return score;
+    }
+}
---
+++ src/test/RulesTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import spil.Account;
+import spil.Die;
+import spil.Player;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RulesTest {
+
+    //Tester at spillerens konto kan komme under 0
+
+    @Test
+    void minimum(){
+        spil.Account testKonto = new Account();
+
+
+
+    }
+
+    //Tester at en spiller vinder ved 3000 points
+
+    //Tester at spilleren får en ekstra tur når der bliver slået 10
+
+}
